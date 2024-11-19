1050049ad3d76ee3da456bbcee9430a784466cc4
LydiaStoneKonstanski <lkonstanski@yahoo.com>
2024-11-19 17:53:38 -0500

    ASCII, Concatenating, exponents, and multistring

---
+++ PythonFocus/ASCII.py
@@ -0,0 +1,24 @@
+
+# First Name: Lydia, Last Name: Stone
+# Fun Fact: I like to dance!
+
+first = """
+L
+L
+L
+L
+L
+LLLLL
+"""
+print(first)
+
+second = """
+  SSS
+ S   S
+ S
+  SSS
+     S
+ S  S
+  SSS   
+"""
+print(second)
