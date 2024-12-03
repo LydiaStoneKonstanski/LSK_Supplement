8726fa5ade38cdc012aa84d6aa8598ded00a76f9
LydiaStoneKonstanski <lkonstanski@yahoo.com>
2024-12-03 17:34:30 -0500

    OOP

--- PythonFocus/functions.py
+++ PythonFocus/functions.py
@@ -140,4 +140,202 @@ print(most_popular3)
 # Venice
 # Florence
 
+def trip_planner_welcome(name):
+  print(f'Welcome to tripplanner v1.0 {name}.')
 
+trip_planner_welcome('Lydia')
+
+def estimated_time_rounded(estimated_time):
+  rounded_time = round(estimated_time)
+  return rounded_time
+
+estimate = estimated_time_rounded(3.5)
+
+def destination_setup(origin, destination, estimated_time, mode_of_transport = 'Car'):
+
+  print(f'Your trip starts off in {origin}')
+  print(f'And you are traveling to {destination}.')
+  print(f'You will be traveling by {mode_of_transport}')
+  print(f'It will take approximately', str(f'{estimated_time}'), 'hours')
+
+destination_setup('Philadelphia', 'Nashville', estimate)
+
+'''Lambda Functions:
+Lambda can put a function inside a variable. Lambda functions are most commonly used as arguments 
+to higher-order functions such as map(), 
+filter(), and sorted(). Higher-order functions are functions that can accept other functions, 
+such as lambda functions, as arguments.'''
+#lambda [arguments]: [expression]
+
+# Regular function
+def square(x):
+    return x ** 2
+
+# Lambda function
+square_lambda = lambda x: x ** 2
+
+# Lambda function to print a name
+#greeting = lambda name: f"Hello, {name}!"
+#print(greeting("Alice"))  # Output: Hello, Alice!
+
+numbers = [1, 2, 3, 4, 5]
+squared = list(map(lambda x: x ** 2, numbers))
+print(squared)  # Output: [1, 4, 9, 16, 25]
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+print(even_numbers)  # Output: [2, 4, 6, 8, 10]
+
+
+students = [('Alice', 'A', 15), ('Bob', 'B', 12), ('Charlie', 'A', 20)]
+sorted_students = sorted(students, key=lambda x: x[2])
+print(sorted_students)
+# Output: [('Bob', 'B', 12), ('Alice', 'A', 15), ('Charlie', 'A', 20)]
+
+# List of numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Use a lambda function to filter out odd numbers
+evens = list(filter(lambda x: x % 2 == 0, numbers))
+
+# Use a lambda function to square each number
+squares = list(map(lambda x: x ** 2, numbers))
+
+# Print the results
+print("Original numbers:", numbers)
+print("Even numbers:", evens)
+print("Squared numbers:", squares)
+
+# Try creating your own lambda function!
+words = [('brick', 29, 'a'),('goggles', 12, 'o'),('apple', 8, 'e'),('produce', 5, 'x')]
+sorted_words = sorted(words, key=lambda x:x[2])
+
+print(sorted_words)
+
+'''map()
+Readability: map() can make our code more concise and easier to read, 
+especially for simple operations on iterables.
+
+Memory Efficiency: map() returns an iterator, which means it doesn’t 
+create the entire result list in memory at once. This can be beneficial when working with large datasets.
+
+Functional Programming: map() encourages a functional programming style, 
+which can lead to more maintainable and testable code.
+
+Remember that map() returns an iterator. If you need a list, wrap it with list().
+
+USE map() WHEN:
+
+We need to apply a function to every item in an iterable.
+We want to transform data without explicitly writing a for loop.
+We’re working with large datasets and want to avoid creating intermediate lists.
+
+While map() is powerful, there also are alternatives:
+
+List Comprehensions: Often more readable for simple operations.
+doubled = [x * 2 for x in numbers] 
+
+Generator Expressions: Similar to list comprehensions but return an iterator.
+doubled = (x * 2 for x in numbers) 
+
+For Loops: More verbose but sometimes clearer for complex operations.
+doubled = [] 
+for x in numbers: 
+   doubled.append(x * 2) '''
+
+
+def double(x):
+    return x * 2
+numbers = [1, 2, 3, 4, 5]
+doubled_numbers = map(double, numbers)
+
+print(list(doubled_numbers))  # Output: [2, 4, 6, 8, 10]
+
+'''Map() with other built-ins'''
+# Converting strings to integers
+str_nums = ['1', '2', '3', '4', '5']
+int_nums = list(map(int, str_nums))
+
+print(int_nums)  # Output: [1, 2, 3, 4, 5]
+
+# Finding the length of strings
+words = ['apple', 'banana', 'cherry']
+word_lengths = list(map(len, words))
+
+print(word_lengths)  # Output: [5, 6, 6]
+
+'''Map() with Lambda functions'''
+numbers = [1, 2, 3, 4, 5]
+doubled = list(map(lambda x: x * 2, numbers))
+
+print(doubled)  # Output: [2, 4, 6, 8, 10]
+
+'''Multiple iterables with map()'''
+list1 = [1, 2, 3]
+list2 = [10, 20, 30]
+result = list(map(lambda x, y: x + y, list1, list2))
+
+print(result)  # Output: [11, 22, 33]
+
+'''Map() to try'''
+# Sample list of names
+names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
+
+# Using map with a lambda function to get the length of each name
+name_lengths = list(map(lambda x: len(x), names))
+
+# Using map with a defined function to capitalize each name
+def capitalize_name(name):
+    return name.upper()
+
+capitalized_names = list(map(capitalize_name, names))
+
+# Print the results
+print("Original names:", names)
+print("Name lengths:", name_lengths)
+print("Capitalized names:", capitalized_names)
+
+'''REVERSE A STRING'''
+backward = list(map(lambda name: name[::-1], names))
+print(backward)
+
+def f_to_c(f_temp):
+  c_temp = (f_temp - 32) * (5/9)
+  return c_temp
+f100_in_celcius = f_to_c(100)
+print(f100_in_celcius)
+
+def c_to_f(c_temp):
+  f_temp = c_temp * (9/5) + 32
+  return f_temp
+c0_in_fahrenheit = c_to_f(0)
+print(c0_in_fahrenheit)
+
+# Uncomment this when you reach the "Use the Force" section
+
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
+
+def get_force(mass, acceleration):
+  return mass * acceleration
+
+train_force = get_force(train_mass, train_acceleration)
+
+print(train_force)
+
+print(f'The GE train supplies {train_force} Newtons of force')
+
+def get_energy(mass, c = 3*10**8):
+  return mass * c**2
+
+bomb_energy = get_energy(bomb_mass)
+print(f'A 1kg bomb supplies {bomb_energy} Joules.')
+
+def get_work(mass, acceleration, distance):
+  return get_force(mass, acceleration) * distance
+
+train_work = get_work(train_mass, train_acceleration, train_distance)
+
+print(f'The GE train does {train_work} Joules of work over {train_distance} meters.')
---
+++ PythonFocus/types.py
@@ -0,0 +1,117 @@
+'''Check Type like this'''
+
+print(type(5))
+
+my_dict = {}
+print(type(my_dict))
+
+my_list = []
+print(type(my_list))
+
+'''Class: To define'''
+class Facade:
+  pass
+
+'''Class must be instantiated, like calling a function. Takes a class and turns it into an object'''
+facade_instance = Facade()
+
+print(type(facade_instance))
+#Prints "class '__main__.Facade'>"
+
+'''Class Variable, a variable available to every instance of the class'''
+class Musician:
+  title = "Rockstar"
+
+drummer = Musician()
+print(drummer.title)
+# prints "Rockstar"
+ '''OR'''
+class Grade:
+  minimum_passing = 65
+
+'''Methods are functions that are part of the larger class.
+The first argument in a method is always the object that is calling the method. 
+Convention recommends that we name this first argument self. 
+Methods always have at least this one argument.
+
+Self is implicitly passed, does not need to be called out'''
+
+class Dog:
+  dog_time_dilation = 7
+
+  def time_explanation(self):
+    print("Dogs experience {} years for every 1 human year.".format(self.dog_time_dilation))
+
+pipi_pitbull = Dog()
+pipi_pitbull.time_explanation()
+# Prints "Dogs experience 7 years for every 1 human year."
+
+class Rules:
+  def washing_brushes(self):
+    return 'Point bristles towards the basin while washing your brushes.'
+
+'''Methods with more than one argument'''
+class DistanceConverter:
+  kms_in_a_mile = 1.609
+  def how_many_kms(self, miles):
+    return miles * self.kms_in_a_mile
+
+converter = DistanceConverter()
+kms_in_5_miles = converter.how_many_kms(5)
+print(kms_in_5_miles)
+# prints "8.045
+
+
+#new class
+class Circle:
+    pi = 3.14
+    #Methods are tabbed in
+    def area(self, radius):
+        return Circle.pi * radius ** 2
+#Instantiated on the class level
+circle = Circle()
+#used on the class level
+pizza_area = circle.area(12 / 2)
+teaching_table_area = circle.area(36 / 2)
+round_room_area = circle.area(11460 / 2)
+
+'''Constructors'''
+
+class Shouter:
+    #uses a 'dunder' CONSTRUCTOR 'double underline'
+    # make sure phrase is a string
+  def __init__(self, phrase):
+    print("HELLO?!")
+    if type(phrase) == str:
+
+      # then shout it out
+      print(phrase.upper())
+
+shout1 = Shouter()
+# prints "HELLO?!"
+
+shout2 = Shouter()
+# prints "HELLO?!"
+
+shout3 = Shouter("shout")
+# prints "SHOUT"
+
+shout4 = Shouter("shout")
+# prints "SHOUT"
+
+shout5 = Shouter("let it all out")
+# prints "LET IT ALL OUT"
+
+class Circle:
+    pi = 3.14
+
+    def __init__(self, diameter):
+        #instantiation
+        self.diameter = diameter
+        print(f"New circle with diameter: {self.diameter}")
+
+
+# Create a circle teaching_table with diameter 36
+teaching_table = Circle(36)
+
+
