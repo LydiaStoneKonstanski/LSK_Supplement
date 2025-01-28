03c9b70a30b5475c4a80ffd63195fc2b9d5a379c
LydiaStoneKonstanski <lkonstanski@yahoo.com>
2025-01-28 14:14:33 -0500

    added several ways to alter columns in working_with_pandas

--- PythonFocus/Pandas/working_with_pandas.py
+++ PythonFocus/Pandas/working_with_pandas.py
@@ -43,3 +43,52 @@ df['Quantity'] = [100, 150, 50, 35]'''
 # )
 #
 # df['Sold in Bulk?'] = ['Yes', 'Yes', 'No', 'No']
+
+'''We can also add a new column that is the same for all rows in the DataFrame: 
+df['In Stock?'] = True
+'''
+
+# import codecademylib3
+# import pandas as pd
+#
+# df = pd.DataFrame([
+#   [1, '3 inch screw', 0.5, 0.75],
+#   [2, '2 inch nail', 0.10, 0.25],
+#   [3, 'hammer', 3.00, 5.50],
+#   [4, 'screwdriver', 2.50, 3.00]
+# ],
+#   columns=['Product ID', 'Description', 'Cost to Manufacture', 'Price']
+# )
+#
+# df['Is taxed?'] = 'Yes'
+#
+# print(df)
+
+
+'''Finally, you can add a new column by performing a function on the existing columns.
+df['Sales Tax'] = df.Price * 0.075
+'''
+
+df = pd.DataFrame([
+  [1, '3 inch screw', 0.5, 0.75],
+  [2, '2 inch nail', 0.10, 0.25],
+  [3, 'hammer', 3.00, 5.50],
+  [4, 'screwdriver', 2.50, 3.00]
+],
+  columns=['Product ID', 'Description', 'Cost to Manufacture', 'Price']
+)
+#Every column name goes in brackets AND quotes
+df['Margin'] = df['Price'] - df['Cost to Manufacture']
+
+'''COLUMN OPERATIONS with .apply
+Use .apply to perform an operation on every line of the column chosen.'''
+df = pd.DataFrame([
+  ['JOHN SMITH', 'john.smith@gmail.com'],
+  ['Jane Doe', 'jdoe@yahoo.com'],
+  ['joe schmo', 'joeschmo@hotmail.com']
+],
+columns=['Name', 'Email'])
+
+df['Lowercase Name'] = df.Name.apply(str.lower)
+
+print(df)
